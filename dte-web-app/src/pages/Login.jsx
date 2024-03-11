a8f2ec74f8291ce5824e032bfb340bb1e64d6f20
luis12007 <00129020@edu.sv>
2024-03-10 22:17:23 -0600

    add sources and view components

--- dte-web-app/src/pages/Login.jsx
+++ dte-web-app/src/pages/Login.jsx
@@ -1,15 +1,36 @@
-import React from "react";
+import SistemaDTE from "../components/SistemDTE";
+import mylogo from "../assets/imgs/mysoftlogo.png";
+import hi from "../assets/imgs/hi.webp";
+import bglogin from "../assets/imgs/bglogin.webp";
 
-import InputLoginComponent from "../components/InputLoginComponent";
-export default function Login() {
+const Login = () => {
   return (
-    <div className="flex justify-center items-center h-screen bg-gray-800">
-      <div className="bg-white rounded-lg shadow-lg p-8 mx-8 h-3/5 w-full md:w-2/4">
-        {/* Content goes here */}
-        <InputLoginComponent></InputLoginComponent>
+    <div className="w-full relative bg-steelblue overflow-hidden flex flex-col items-center justify-start pt-[15px] px-0 pb-0 box-border gap-[55px_0px] tracking-[normal]">
+      <div className="self-stretch flex flex-row items-start justify-center py-0 px-5">
+        <img
+          className="h-[107px] w-[149px] relative object-cover"
+          loading="lazy"
+          alt=""
+          src={mylogo}
+        />
       </div>
+      <main className="self-stretch flex flex-col items-center justify-center h-[753px] relative max-w-full">
+        <SistemaDTE />
+        <section className="absolute top-[170px] left-[0px] w-[430px] h-[583px]">
+          <img
+            className="absolute top-[0px] left-[-185px] w-[811px] h-[827px] object-cover"
+            alt=""
+            src={bglogin}
+          />
+          <img
+            className="relative top-[185px] left-[-55px] w-[324px] h-[457px] object-cover z-[2]"
+            alt=""
+            src={hi}
+          />
+        </section>
+      </main>
     </div>
   );
-}
+};
 
-/* https://react-icons.github.io/react-icons/icons/ai/ */
+export default Login;
--- dte-web-app/tailwind.config.js
+++ dte-web-app/tailwind.config.js
@@ -1,10 +1,36 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
-  purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
-
+    content: ["./src/**/*.{js,jsx,ts,tsx}"],
+    theme: {
+        extend: {
+            colors: {
+                steelblue: "#395576",
+                white: "#fff",
+                slategray: "#5e7a9c",
+                deepskyblue: "#63b4ff",
+                lavender: "#e6eaff",
+                gray: "#87827f",
+                black: "#000",
+            },
+            spacing: {},
+            fontFamily: {
+                "inria-sans": "'Inria Sans'",
+                inter: "Inter",
+            },
+            borderRadius: {
+                mini: "15px",
+                "3xs": "10px",
+            },
+        },
+        fontSize: {
+            lg: "18px",
+            xs: "12px",
+            mini: "15px",
+            "3xl": "22px",
+            inherit: "inherit",
+        },
+    },
+    corePlugins: {
+        preflight: false,
+    },
+};
