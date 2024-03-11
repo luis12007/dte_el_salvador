5c856040b2e4245f17392fba362a6f2928815152
luis12007 <00129020@edu.sv>
2024-03-10 22:16:15 -0600

    add login view

--- dte-web-app/src/components/InputLoginComponent.jsx
+++ dte-web-app/src/components/InputLoginComponent.jsx
@@ -1,14 +1,13 @@
 import React from 'react';
 
-const InputLoginComponent = ({ icon, ...rest }) => {
+const InputLoginComponent = ({placeholder, icon, onchange }) => {
   return (
     <div className="relative">
       <input
-        className="border border-gray-300 rounded-lg px-4 py-2 pl-10 focus:outline-none focus:border-gray-500 hover:border-gray-500"
-        {...rest}
+        className="border border-gray-300 rounded-lg px-4 py-2 pl-10 focus:outline-none focus:border-gray-500 hover:border-gray-500" placeholder={placeholder} onChange={onchange}
       />
-      <div className="absolute left-3 top-1/2 transform -translate-y-1/2">
-        {icon}
+      <div className="absolute left-2 top-1/2 transform -translate-y-1/2">
+        <img className='h-6 ' src={icon} alt="icon" />
       </div>
     </div>
   );
